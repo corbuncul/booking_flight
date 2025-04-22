ff475b914da37ae2de70c32c97bdcb567302f4e5
corbuncul <agafivan@gmai.com>
2025-04-22 22:39:18 +0300

    edit CRUD ticket, flight, passenger, route

--- app/crud/route.py
+++ app/crud/route.py
@@ -10,19 +10,19 @@ from app.schemas.route import RouteDB
 
 class CRUDRoute(CRUDBase):
 
-    async def get_routes_by_towns(
+    async def get_routes_by_cities(
         self,
         session: AsyncSession,
         *,
-        from_town: str | None = None,
-        to_town: str | None = None
+        from_city: str | None = None,
+        to_city: str | None = None
     ) -> list[Optional[RouteDB]]:
         """Получение маршрутов по городам."""
         query = select(self.model)
-        if from_town is not None:
-            query = query.where(self.model.from_town == from_town)
-        if to_town is not None:
-            query = query.where(self.model.to_town == to_town)
+        if from_city is not None:
+            query = query.where(self.model.from_city == from_city)
+        if to_city is not None:
+            query = query.where(self.model.to_city == to_city)
         db_routes = await session.execute(query)
         return db_routes.scalars().all()
 
