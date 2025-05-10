222be947ae2aa7439e19a6e3b0ae03e22f9efe5d
corbuncul <agafivan@gmail.com>
2025-05-11 01:39:01 +0300

    add Discount model to __init__.py in models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,4 +1,5 @@
 from .city import City  # noqa: F401
+from .discount import Discount  # noqa: F401
 from .flight import Flight  # noqa: F401
 from .flightcity import FlightCity  # noqa: F401
 from .passenger import Passenger  # noqa: F401
