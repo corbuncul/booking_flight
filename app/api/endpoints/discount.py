0cf1657dfd4aac6995873cba40e8f3a59c2bc136
Corbuncul <agafivan@gmail.com>
2025-05-15 00:01:25 +0300

    edit routes, add discount, routecost, flightcity routes. add validators

---
+++ app/api/endpoints/discount.py
@@ -0,0 +1,71 @@
+from fastapi import APIRouter, Depends
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+from app.api.validators import (
+    check_discount_exists,
+)
+from app.core.db import get_async_session
+from app.core.user import current_superuser
+from app.crud import discount_crud
+from app.schemas.discount import DiscountCreate, DiscountDB, DiscountUpdate
+
+router = APIRouter()
+
+
+@router.get(
+    '/',
+    response_model=list[DiscountDB],
+    dependencies=[Depends(current_superuser)],
+)
+async def get_all_discounts(
+    session: AsyncSession = Depends(get_async_session),
+):
+    """Получение всех скидок. Только для суперюзеров."""
+    return await discount_crud.get_all(session)
+
+
+@router.post(
+    '/',
+    response_model=DiscountDB,
+    dependencies=[Depends(current_superuser)],
+)
+async def create_new_discount(
+    discount: DiscountCreate,
+    session: AsyncSession = Depends(get_async_session),
+):
+    """Создание скидки."""
+    new_discount = await discount_crud.create(discount, session)
+    return new_discount
+
+
+@router.patch(
+    '/{discount_id}',
+    response_model=DiscountDB,
+    dependencies=[Depends(current_superuser)],
+)
+async def update_discount(
+    discount_id: int,
+    obj_in: DiscountUpdate,
+    session: AsyncSession = Depends(get_async_session),
+):
+    """Обновление скидки."""
+    discount = await check_discount_exists(session, discount_id)
+    db_discount = await discount_crud.update(discount, obj_in, session)
+    return db_discount
+
+
+@router.delete(
+    '/{route_id}',
+    response_model=DiscountDB,
+    dependencies=[Depends(current_superuser)],
+)
+async def delete_discount(
+    discount_id: int,
+    session: AsyncSession = Depends(get_async_session),
+):
+    """Удаление скидки."""
+    discount = await check_discount_exists(session, discount_id)
+    db_discount = await discount_crud.remove(discount, session)
+    return db_discount
---
+++ app/api/endpoints/routecost.py
@@ -0,0 +1,82 @@
+from fastapi import APIRouter, Depends
+
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+from app.api.validators import check_routecost_exists, check_routecost_duplicate
+from app.core.db import get_async_session
+from app.core.user import current_superuser
+from app.crud import routecost_crud
+from app.schemas.routecost import RouteCostCreate, RouteCostResponse, RouteCostUpdate
+
+
+router = APIRouter()
+
+
+@router.get(
+    '/',
+    response_model=list[RouteCostResponse],
+    dependencies=[Depends(current_superuser)],
+)
+async def get_all_routes(
+    session: AsyncSession = Depends(get_async_session),
+):
+    """Список всех маршрутов. Только для суперюзеров."""
+    return await routecost_crud.get_all(session)
+
+
+@router.post(
+    '/',
+    response_model=RouteCostResponse,
+    dependencies=[Depends(current_superuser)],
+)
+async def create_new_route(
+    routecost: RouteCostCreate,
+    session: AsyncSession = Depends(get_async_session),
+):
+    """Создание маршрута."""
+    new_routecost = await routecost_crud.create(routecost, session)
+    return new_routecost
+
+
+@router.patch(
+    '/{routecost_id}',
+    response_model=RouteCostResponse,
+    dependencies=[Depends(current_superuser)],
+)
+async def update_route(
+    routecost_id: int,
+    obj_in: RouteCostUpdate,
+    session: AsyncSession = Depends(get_async_session),
+):
+    """Обновление маршрута."""
+    routecost = await check_routecost_exists(session, routecost_id)
+    if (obj_in.from_city is not None) and (obj_in.to_city is not None):
+        await check_routecost_duplicate(
+            session, from_city=obj_in.from_city, to_city=obj_in.to_city
+        )
+    if obj_in.from_city is not None:
+        await check_routecost_duplicate(
+            session, from_city=obj_in.from_city, to_city=routecost.to_city
+        )
+    if obj_in.to_city is not None:
+        await check_routecost_duplicate(
+            session, from_city=routecost.from_city, to_city=obj_in.to_city
+        )
+    db_routecost = await routecost_crud.update(routecost, obj_in, session)
+    return db_routecost
+
+
+@router.delete(
+    '/{routecost_id}',
+    response_model=RouteCostResponse,
+    dependencies=[Depends(current_superuser)],
+)
+async def delete_route(
+    routecost_id: int,
+    session: AsyncSession = Depends(get_async_session),
+):
+    """Удаление маршрута."""
+    routecost = await check_routecost_exists(session, routecost_id)
+    db_routecost = await routecost_crud.remove(routecost, session)
+    return db_routecost
